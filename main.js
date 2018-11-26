ad6846c2c4f5872aa516543e9bcfbf76907190bd
Vladimir M <vov4ikm1@gmail.com>
2018-11-26 17:28:11 +0200

    inheritance circle

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ class Shape {
     }
 }
 
-class Circle {
+class Circle  {
     constructor(diameter){
        this.diameter = diameter;
     }
@@ -30,19 +30,18 @@ class Circle {
     } 
 }
 
-class triangle {
-    constructor(a,b,c){        
-        this.a  = a;
-        this.b = b;
+class Triangle extends Shape {
+    constructor(width,height,c){
+        super(width , height)        
         this.c = c;
     }
     
     square(){        
-        console.log("triangle square = " + ( (this.a/2) * this.b))    
+        console.log("triangle square = " + ( (this.width/2) * this.height))    
     }
 
     perimeter(){       
-        console.log("triangle perimeter = " + (this.a + this.b + this.c))
+        console.log("triangle perimeter = " + (this.width + this.height + this.c))
     } 
 }
 
@@ -51,47 +50,15 @@ s.square()
 s.perimeter()
 
 var circl = new Circle(6);
-
 circl.square()
 circl.perimeter()
 
-
-
-
-
-// class Persone {
-//     constructor(firstName, age){
-//     this.firstName = firstName;
-//      this._age = age
-//     // var age = age
-//     // this.getAge = function(){
-//     //       return age
-//     // } 
-//     }
-//     walk() {
-//           console.log('I am walking !');
-//     };
-    
-//     sayHello() {
-//           console.log("Hello , i'm" + this.firstName);
-//     };
-// };
-
-// class Student extends Persone{
-//     constructor(firstName , age, subject){
-//         super(firstName, age);  //ссілка на наследуемій класс
-//         this.subject = subject
-//     }
-//     sayHello (){
-//           console.log("Hello , i'm" + this.firstName + "i'm studing" + this.subject)
-//     } 
-// }
+var triangle = new Triangle (4 , 7 , 8);
+triangle.square()
+triangle.perimeter()
 
 
 
 
 
 
-// var user1 = new Student('John' ,43, 'FEP')
-
-// console.log(user1)
